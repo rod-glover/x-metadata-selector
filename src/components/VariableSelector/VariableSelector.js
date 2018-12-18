bdd11d6f1d133d9ad8a9dd7955797fa2f991d97f
Rod Glover <rglover@uvic.ca>
2018-12-17 18:06:04 -0800

    Add MYM flag

--- src/components/VariableSelector/VariableSelector.js
+++ src/components/VariableSelector/VariableSelector.js
@@ -3,7 +3,9 @@ import React from 'react';
 import Select from 'react-select';
 
 import flow from 'lodash/fp/flow';
+import identity from 'lodash/fp/identity';
 import map from 'lodash/fp/map';
+import join from 'lodash/fp/join';
 import pick from 'lodash/fp/pick';
 import sortBy from 'lodash/fp/sortBy';
 import uniqBy from 'lodash/fp/uniqBy';
@@ -15,15 +17,19 @@ export default class VariableSelector extends React.Component {
     meta: PropTypes.array,
   };
 
-  optionValueToLabel =
-    ({ variable_id, variable_name }) => `${variable_id} - ${variable_name}`;
+  static variable_props =
+    'variable_id variable_name multi_year_mean'.split(' ');
+
+  // concatProps = obj => flow(map(identity), join(' '))(obj);
+  optionValueToLabel = ({ variable_id, variable_name, multi_year_mean }) =>
+    `(${multi_year_mean ? '' : 'not '}MYM) ${variable_id} - ${variable_name}`;
   getOptionLabel = option => this.optionValueToLabel(option.value);
 
   render() {
     const options = flow(
       uniqBy(this.optionValueToLabel),
       map(m => (
-        { value: pick(['variable_id', 'variable_name'])(m) }
+        { value: pick(VariableSelector.variable_props)(m) }
       )),
       sortBy('value.variable_id'),
     )(this.props.meta);
