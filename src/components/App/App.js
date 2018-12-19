acbd048cfc129a3de8a246c39e1bf5070ac8b685
Rod Glover <rglover@uvic.ca>
2018-12-19 15:52:18 -0800

    Use MetadataSelector for model selection

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -6,12 +6,37 @@ import meta from '../../assets/meta'
 import './App.css';
 import VariableSelector from '../VariableSelector';
 import ModelSelector from '../ModelSelector';
+import MetadataSelector from '../MetadataSelector';
+import PropTypes from 'prop-types';
 
 function stringify(obj) {
   return <pre>{JSON.stringify(obj, null, 2)}</pre>;
 }
 
-class App extends Component {
+
+class SpicyModelSelector extends Component {
+  static getOptionValue = metadatum => metadatum.model_id;
+
+  render() {
+    return (
+      <MetadataSelector
+        {...this.props}
+        getOptionValue={SpicyModelSelector.getOptionValue}
+      />
+    );
+  }
+}
+
+
+class SpicyVariableSelector extends Component {
+  render() {
+    return <div>SpicyVariableSelector</div>
+  }
+}
+
+
+
+  class App extends Component {
   state = {
     model: 'MRI-CGCM3',
     variable: null,
@@ -26,6 +51,9 @@ class App extends Component {
     return (
       <Grid fluid>
         <Row>
+          <Col lg={3}>
+            Original Flavour
+          </Col>
           <Col lg={3}>
             <ModelSelector
               meta={meta}
@@ -43,6 +71,39 @@ class App extends Component {
           </Col>
         </Row>
         <Row>
+          <Col lg={3}>
+          </Col>
+          <Col lg={3}>
+            {stringify(this.state.model)}
+          </Col>
+          <Col lg={3}>
+            {stringify(this.state.variable)}
+          </Col>
+        </Row>
+
+        <Row>
+          <Col lg={3}>
+            Spicy
+          </Col>
+          <Col lg={3}>
+            <SpicyModelSelector
+              meta={meta}
+              value={this.state.model}
+              onChange={this.handleChangeModel}
+            />
+          </Col>
+          <Col lg={3}>
+            <SpicyVariableSelector
+              meta={meta}
+              constraint={this.variableSelectorConstraint(this.state.model)}
+              value={this.state.variable}
+              onChange={this.handleChangeVariable}
+            />
+          </Col>
+        </Row>
+        <Row>
+          <Col lg={3}>
+          </Col>
           <Col lg={3}>
             {stringify(this.state.model)}
           </Col>
