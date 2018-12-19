d84adc8a33ed9733040e7869e12dd5162bb429e8
Rod Glover <rglover@uvic.ca>
2018-12-19 11:29:53 -0800

    Add selector for model and use for variable constraint

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Grid, Row, Col } from 'react-bootstrap';
+import memoize from 'memoize-one';
 
 import meta from '../../assets/meta'
 import './App.css';
 import VariableSelector from '../VariableSelector';
+import ModelSelector from '../ModelSelector';
 
 function stringify(obj) {
   return <pre>{JSON.stringify(obj, null, 2)}</pre>;
@@ -11,26 +13,39 @@ function stringify(obj) {
 
 class App extends Component {
   state = {
+    model: 'MRI-CGCM3',
     variable: null,
-    constraint: { model_id: 'MRI-CGCM3' },
   };
 
+  variableSelectorConstraint = memoize(model_id => ({ model_id }));
+
   handleChangeVariable = variable => this.setState({ variable });
+  handleChangeModel = model => this.setState({ model });
 
   render() {
     return (
       <Grid fluid>
         <Row>
           <Col lg={3}>
+            <ModelSelector
+              meta={meta}
+              value={this.state.model}
+              onChange={this.handleChangeModel}
+            />
+          </Col>
+          <Col lg={3}>
             <VariableSelector
               meta={meta}
-              constraint={this.state.constraint}
+              constraint={this.variableSelectorConstraint(this.state.model)}
               value={this.state.variable}
               onChange={this.handleChangeVariable}
             />
           </Col>
         </Row>
         <Row>
+          <Col lg={3}>
+            {stringify(this.state.model)}
+          </Col>
           <Col lg={3}>
             {stringify(this.state.variable)}
           </Col>
