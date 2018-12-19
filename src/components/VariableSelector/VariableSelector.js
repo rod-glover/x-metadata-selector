7fdabe269b97ff6a14732778518ccf89dd26a7db
Rod Glover <rglover@uvic.ca>
2018-12-18 17:42:24 -0800

    Memoize option generation (expensive computation)

--- src/components/VariableSelector/VariableSelector.js
+++ src/components/VariableSelector/VariableSelector.js
@@ -3,6 +3,8 @@ import React from 'react';
 import Select, { components } from 'react-select';
 import { Glyphicon } from 'react-bootstrap';
 
+import memoize from "memoize-one";
+
 import flow from 'lodash/fp/flow';
 import identity from 'lodash/fp/identity';
 import map from 'lodash/fp/map';
@@ -19,7 +21,7 @@ import './VariableSelector.css';
 const { Option } = components;
 
 const MyOption = props => {
-  console.log('MyOption', props)
+  // console.log('MyOption', props)
   return (
   <Option {...props}>
     <Glyphicon glyph={props.value.multi_year_mean ? 'repeat' : 'star'}/>
@@ -39,12 +41,14 @@ export default class VariableSelector extends React.Component {
   static variable_props =
     'variable_id variable_name multi_year_mean'.split(' ');
 
-
   optionValueToLabel = ({ variable_id, variable_name }) =>
     `${variable_id} - ${variable_name}`;
 
-  render() {
-    const allOptions = flow(
+  // Memoize computation of options list
+  // See https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html#what-about-memoization
+
+  allOptions = memoize(
+    (meta) => flow(
       map(pick(VariableSelector.variable_props)),
       uniqBy(JSON.stringify),
       map(value => (
@@ -54,24 +58,37 @@ export default class VariableSelector extends React.Component {
         }
       )),
       sortBy('value.variable_id'),
-    )(this.props.meta);
-    const mymOptions = filter(o => o.value.multi_year_mean)(allOptions);
-    const notMymOptions = filter(o => !o.value.multi_year_mean)(allOptions);
-    const options = [
+    )(meta)
+  );
+
+  // Doesn't need to be memoized
+  mymOptions = memoize(
+    (meta) => filter(o => o.value.multi_year_mean)(this.allOptions(meta))
+  );
+
+  // Doesn't need to be memoized
+  notMymOptions = memoize(
+    (meta) => filter(o => !o.value.multi_year_mean)(this.allOptions(meta))
+  );
+
+  options = memoize(
+    (meta) => [
       {
         label: 'Multi-Year Mean Datasets',
-        options: mymOptions,
+        options: this.mymOptions(meta),
       },
       {
         label: 'Time Series Datasets',
-        options: notMymOptions,
+        options: this.notMymOptions(meta),
       },
-    ];
+    ]
+  );
 
+  render() {
     return (
       <Select
         isSearchable
-        options={options}
+        options={this.options(this.props.meta)}
         components={{ Option: MyOption }}
         value={this.props.value}
         onChange={this.props.onChange}
