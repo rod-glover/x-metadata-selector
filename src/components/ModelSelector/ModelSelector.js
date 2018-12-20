2007f65ccea5b521714cd2e28b93ebc6aa900771
Rod Glover <rglover@uvic.ca>
2018-12-20 10:02:45 -0800

    Create new ModelSelector specializing ConstrainedMetadataSelector

--- src/components/ModelSelector/ModelSelector.js
+++ src/components/ModelSelector/ModelSelector.js
@@ -1,64 +1,23 @@
 import PropTypes from 'prop-types';
-import React from 'react';
-import Select, { components } from 'react-select';
+import React, { Component } from 'react';
 
-import memoize from 'memoize-one';
-
-import {
-  curry,
-  flow,
-  identity,
-  isMatch,
-  map,
-  filter,
-  find,
-  pick,
-  sortBy,
-  sortedUniqBy,
-  groupBy,
-  toPairs,
-  some,
-  tap,
-} from 'lodash/fp'
-
-import objectId from '../../debug-utils/object-id';
+import ConstrainedMetadataSelector from '../ConstrainedMetadataSelector';
 
 import './ModelSelector.css';
 
 
-export default class ModelSelector extends React.Component {
+export default class ModelSelector extends Component {
   static propTypes = {
-    meta: PropTypes.array,
-    value: PropTypes.any,
-    onChange: PropTypes.func,
+    constraint: PropTypes.object,
   };
 
-  options = memoize(
-    meta => flow(
-      tap(meta => console.log('MS.options: meta:', objectId(meta))),
-      map(m => ({
-        value: m.model_id,
-        label: m.model_id,
-      })),
-      sortBy('value'),
-      sortedUniqBy('value'),
-    )(meta)
-  );
-
-  // Value-exchange functions
-
-  optionFor = value => find({ value })(this.options(this.props.meta));
-
-  handleChange = option => this.props.onChange(option.value);
+  static getOptionValue = metadatum => metadatum.model_id;
 
   render() {
-    console.log('MS.render')
     return (
-      <Select
-        isSearchable
-        options={this.options(this.props.meta)}
-        value={this.optionFor(this.props.value)}
-        onChange={this.handleChange}
+      <ConstrainedMetadataSelector
+        {...this.props}
+        getOptionValue={ModelSelector.getOptionValue}
       />
     );
   }
