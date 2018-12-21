7fda0f6060aefdaf6dc0dbced497fadd99294e7a
Rod Glover <rglover@uvic.ca>
2018-12-20 16:28:05 -0800

    Add component EmissionsScenarioSelector

---
+++ src/components/EmissionsScenarioSelector/EmissionsScenarioSelector.js
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import ConstrainedMetadataSelector from '../ConstrainedMetadataSelector';
+
+import './EmissionsScenarioSelector.css';
+
+
+export default class EmissionsScenarioSelector extends Component {
+  static propTypes = {
+    constraint: PropTypes.object,
+  };
+
+  static getOptionValue = metadatum => metadatum.experiment;
+
+  render() {
+    return (
+      <ConstrainedMetadataSelector
+        {...this.props}
+        getOptionValue={EmissionsScenarioSelector.getOptionValue}
+      />
+    );
+  }
+}
