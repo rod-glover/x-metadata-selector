1c3f9e0e6a29cc3b6d21f11141b51d4ef3612d77
Rod Glover <rglover@uvic.ca>
2018-12-21 16:32:26 -0800

    Create component DatasetSelector

---
+++ src/components/DatasetSelector/DatasetSelector.js
@@ -0,0 +1,32 @@
+import PropTypes from 'prop-types';
+import React, { Component } from 'react';
+
+import ConstrainedMetadataSelector from '../ConstrainedMetadataSelector';
+
+import './DatasetSelector.css';
+import { pick } from 'lodash/fp';
+
+
+export default class DatasetSelector extends Component {
+  static propTypes = {
+    constraint: PropTypes.object,
+  };
+
+  static valueProps =
+    'start_date end_date ensemble_member'.split(' ');
+  static getOptionValue = metadatum =>
+    pick(DatasetSelector.valueProps, metadatum);
+
+  static getOptionLabel = ({ value: { start_date, end_date, ensemble_member }}) =>
+    `${ensemble_member} ${start_date}-${end_date}`;
+  
+  render() {
+    return (
+      <ConstrainedMetadataSelector
+        {...this.props}
+        getOptionValue={DatasetSelector.getOptionValue}
+        getOptionLabel={DatasetSelector.getOptionLabel}
+      />
+    );
+  }
+}
