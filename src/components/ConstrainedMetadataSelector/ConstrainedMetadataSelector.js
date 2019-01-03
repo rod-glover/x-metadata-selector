bf017392f8cbbcc8a628381b5c6f1666daaa7f6a
Rod Glover <rglover@uvic.ca>
2019-01-03 15:53:56 -0800

    Purify ConstrainedMetadataSelector getOptionIsDisabled

--- src/components/ConstrainedMetadataSelector/ConstrainedMetadataSelector.js
+++ src/components/ConstrainedMetadataSelector/ConstrainedMetadataSelector.js
@@ -12,16 +12,28 @@ export default class ConstrainedMetadataSelector extends React.Component {
     debugValue: PropTypes.any,
   };
 
-  // Can't be static: needs to access props.
-  getOptionIsDisabled = option => !some(
-    context => isMatch(this.props.constraint, context)
-  )(option.contexts);
+  makeGetOptionIsDisabled = constraint => (
+    // Returns a `getOptionIsDisabled` function based on the passed in
+    // constraint. It is important that the function be a distinct function
+    // for each constraint, instead of the same function (object) that closes
+    // over `this.props.constraint`. The change in prop (`getOptionIsDisabeled`)
+    // tells `MetadataSelector` that it must re-render. (Otherwise it has the
+    // option not to, which in this case it takes, causing an error if the
+    // same (ref), but differently-behaving function is passed in.)
+    //
+    // (This is a place where JS's otherwise good FP design falls down,
+    // and comes down to the broader shortcoming that objects are mutable.)
+    option => !some(
+      context => isMatch(this.props.constraint, context)
+    )(option.contexts)
+
+  );
 
   render() {
     return (
       <MetadataSelector
         {...omit('constraint', this.props)}
-        getOptionIsDisabled={this.getOptionIsDisabled}
+        getOptionIsDisabled={this.makeGetOptionIsDisabled(this.props.constraint)}
       />
     );
   }
