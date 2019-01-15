e6fe619eb38a71672c68374a9562e0b4fcbd1b1e
Rod Glover <rglover@uvic.ca>
2019-01-15 13:45:42 -0800

    Memoize makeGetOptionIsDisabled

--- src/components/ConstrainedMetadataSelector/ConstrainedMetadataSelector.js
+++ src/components/ConstrainedMetadataSelector/ConstrainedMetadataSelector.js
@@ -3,6 +3,7 @@ import React from 'react';
 
 import './ConstrainedMetadataSelector.css';
 import { isMatch, some, omit } from 'lodash/fp';
+import memoize from 'memoize-one';
 import MetadataSelector from '../GroupingSelector';
 
 
@@ -12,22 +13,25 @@ export default class ConstrainedMetadataSelector extends React.Component {
     debugValue: PropTypes.any,
   };
 
-  makeGetOptionIsDisabled = constraint => (
+  makeGetOptionIsDisabled = memoize(constraint => (
     // Returns a `getOptionIsDisabled` function based on the passed in
     // constraint. It is important that the function be a distinct function
     // for each constraint, instead of the same function (object) that closes
-    // over `this.props.constraint`. The change in prop (`getOptionIsDisabeled`)
-    // tells `GroupingSelector` that it must re-render. (Otherwise it has the
+    // over the mutable `this.props.constraint`. The change in the value of
+    // `getOptionIsDisabled` tells `GroupingSelector` that it must re-render.
+    // (Otherwise it has the
     // option not to, which in this case it takes, causing an error if the
-    // same (ref), but differently-behaving function is passed in.)
+    // same-identity, but differently-behaving function is passed in.)
     //
     // (This is a place where JS's otherwise good FP design falls down,
     // and comes down to the broader shortcoming that objects are mutable.)
+    //
+    // This function is memoized to eliminate false signals that the constraint
+    // has changed.
     option => !some(
       context => isMatch(this.props.constraint, context)
     )(option.contexts)
-
-  );
+  ));
 
   render() {
     return (
